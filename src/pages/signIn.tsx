76bc0f00f1c7a5341ad4507e347970f2db4dfcbc
abangon <novotrek@gmail.com>
2025-02-22 10:52:23 +0100

    Update signIn.tsx

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const SignIn: React.FC = () => {
-  return <h1>Sign In Page</h1>;
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <p>Login to access your profile and race stats.</p>
+    </div>
+  );
 };
 
 export default SignIn;

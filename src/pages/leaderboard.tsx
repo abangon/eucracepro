beaa2890402847dda37509ea440325a7ec329f93
abangon <novotrek@gmail.com>
2025-02-22 10:52:06 +0100

    Update leaderboard.tsx

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Leaderboard: React.FC = () => {
-  return <h1>Leaderboard Page</h1>;
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <p>Check out the rankings of the top racers.</p>
+    </div>
+  );
 };
 
 export default Leaderboard;

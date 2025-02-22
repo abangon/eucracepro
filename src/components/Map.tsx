9d6c96310283dce29481162644a1569dc30714c4
abangon <novotrek@gmail.com>
2025-02-22 18:34:46 +0100

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -35,7 +35,7 @@ const Map: React.FC = () => {
         <Typography variant="subtitle2" color="text.secondary" gutterBottom>
           🌍 Registered Racers by Country
         </Typography>
-        <Box sx={{ width: "100%", height: "100%" }}>
+        <Box sx={{ width: "100%", height: 300 }}>
           <ComposableMap projectionConfig={{ scale: 220 }}>
             <Geographies geography={geoUrl}>
               {({ geographies }) =>

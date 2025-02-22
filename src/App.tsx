9f2089d64f620f4cd0d354c0bfd29a463cfce375
abangon <novotrek@gmail.com>
2025-02-22 10:57:31 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme/theme';
-import Sidebar from './components/sidebar';  // Импортируем Sidebar
-import Navbar from './components/navbar';  // Импортируем Navbar
+import Sidebar from './components/sidebar';
+import Navbar from './components/navbar';
 import Home from './pages/home';
 import Races from './pages/races';
 import Leaderboard from './pages/leaderboard';
 import SignIn from './pages/signIn';
+import { Box } from '@mui/material';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <div style={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex' }}>
           <Sidebar />
-          <div style={{ flexGrow: 1 }}>
+          <Box sx={{ flexGrow: 1 }}>
             <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/races" element={<Races />} />
-              <Route path="/leaderboard" element={<Leaderboard />} />
-              <Route path="/sign-in" element={<SignIn />} />
-            </Routes>
-          </div>
-        </div>
+            <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8 }}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/races" element={<Races />} />
+                <Route path="/leaderboard" element={<Leaderboard />} />
+                <Route path="/sign-in" element={<SignIn />} />
+              </Routes>
+            </Box>
+          </Box>
+        </Box>
       </Router>
     </ThemeProvider>
   );

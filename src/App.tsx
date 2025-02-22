4f40d0c84bb9b24def3a67df6245dcd3362be9ca
abangon <novotrek@gmail.com>
2025-02-22 10:49:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme/theme';
+import Sidebar from './components/sidebar';  // Импортируем Sidebar
+import Navbar from './components/navbar';  // Импортируем Navbar
 import Home from './pages/home';
 import Races from './pages/races';
 import Leaderboard from './pages/leaderboard';
@@ -13,12 +15,18 @@ const App: React.FC = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/races" element={<Races />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/sign-in" element={<SignIn />} />
-        </Routes>
+        <div style={{ display: 'flex' }}>
+          <Sidebar />
+          <div style={{ flexGrow: 1 }}>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/races" element={<Races />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
+              <Route path="/sign-in" element={<SignIn />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </ThemeProvider>
   );

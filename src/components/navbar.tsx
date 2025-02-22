b938f492f1a9ce4a9c0d6a0e09955f2f37a82367
abangon <novotrek@gmail.com>
2025-02-22 10:57:17 +0100

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Avatar } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Navbar: React.FC = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="fixed" sx={{ width: `calc(100% - 240px)`, ml: '240px' }}>
       <Toolbar>
-        <Typography variant="h6">EUC Race Pro</Typography>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          EUC Race Pro
+        </Typography>
+        <IconButton color="inherit">
+          <SettingsIcon />
+        </IconButton>
+        <Avatar sx={{ ml: 2 }} />
       </Toolbar>
     </AppBar>
   );

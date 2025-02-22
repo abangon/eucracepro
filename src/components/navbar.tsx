e8437f83c5fe15a13cc17ece7b2b967aebd3d3e2
abangon <novotrek@gmail.com>
2025-02-22 12:19:43 +0100

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,16 @@
+// src/components/navbar.tsx
 import React from 'react';
 import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar: React.FC = () => {
   return (
-    <AppBar
-      position="static"
-      color="primary"
-      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-    >
+    <AppBar position="static" color="inherit" sx={{ borderBottom: '1px solid #e0e0e0' }}>
       <Toolbar>
-        <IconButton
-          color="inherit"
-          edge="start"
-          sx={{ mr: 2, display: { md: 'none' } }}
-        >
+        <IconButton color="primary" edge="start" sx={{ mr: 2, display: { md: 'none' } }}>
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap>
+        <Typography variant="h6" color="primary" noWrap>
           EuCracePro
         </Typography>
       </Toolbar>

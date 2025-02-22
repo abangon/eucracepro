772e6512dbd0289fcb3502b1683075fca962696a
abangon <novotrek@gmail.com>
2025-02-22 13:01:31 +0100

    Update sidebar.tsx

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,21 @@
 // src/components/sidebar.tsx
 import React from 'react';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, Divider } from '@mui/material';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Divider,
+  Box
+} from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import LoginIcon from '@mui/icons-material/Login';
 import { useNavigate } from 'react-router-dom';
+import logo from '../images/logo/eucrace-logo.jpg';
 
 const drawerWidth = 240;
 
@@ -28,12 +38,20 @@ const Sidebar: React.FC = () => {
         [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
       }}
     >
-      <Toolbar />
+      <Toolbar>
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+          <img
+            src={logo}
+            alt="EuCrace Logo"
+            style={{ width: '80%', objectFit: 'contain' }}
+          />
+        </Box>
+      </Toolbar>
       <Divider />
       <List>
         {menuItems.map((item) => (
           <ListItem button key={item.text} onClick={() => navigate(item.path)}>
-            <ListItemIcon sx={{ color: theme => theme.palette.primary.main }}>{item.icon}</ListItemIcon>
+            <ListItemIcon>{item.icon}</ListItemIcon>
             <ListItemText primary={item.text} />
           </ListItem>
         ))}

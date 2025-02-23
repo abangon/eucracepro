50e2a24034e430d5fd7cb3dec30e64aa6291500e
abangon <novotrek@gmail.com>
2025-02-23 15:27:31 +0100

    Create CountryFlag.tsx

---
+++ src/components/CountryFlag.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Box } from "@mui/material";
+import ReactCountryFlag from "react-country-flag";
+
+interface CountryFlagProps {
+  countryCode: string;
+}
+
+const CountryFlag: React.FC<CountryFlagProps> = ({ countryCode }) => {
+  return (
+    <Box
+      sx={{
+        width: "2em",
+        height: "2em",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: "50%",
+        overflow: "hidden",
+      }}
+    >
+      <ReactCountryFlag
+        countryCode={countryCode}
+        svg
+        style={{ width: "100%", height: "100%" }}
+        title={countryCode}
+      />
+    </Box>
+  );
+};
+
+export default CountryFlag;

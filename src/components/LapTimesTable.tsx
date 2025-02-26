8f1f2a0007b94e22e72585f37f83a942118c0829
abangon <novotrek@gmail.com>
2025-02-26 15:22:57 +0100

    Update LapTimesTable.tsx

--- src/components/LapTimesTable.tsx
+++ src/components/LapTimesTable.tsx
@@ -1,10 +1,18 @@
-// src/components/LapTimesTable.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Typography, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig"; // Импорт Firestore
 
 interface LapTime {
   lap: number;
-  time: string; // например, "1:23.456"
+  time: string;
+  chipNumber: string;
+}
+
+interface Racer {
+  chipNumber: string;
+  nickname: string;
+  raceNumber: string;
 }
 
 interface LapTimesTableProps {
@@ -12,6 +20,25 @@ interface LapTimesTableProps {
 }
 
 const LapTimesTable: React.FC<LapTimesTableProps> = ({ lapTimes }) => {
+  const [racers, setRacers] = useState<Record<string, Racer>>({});
+
+  useEffect(() => {
+    const fetchRacers = async () => {
+      const racersCollection = collection(db, "races", "8915", "participants");
+      const querySnapshot = await getDocs(racersCollection);
+      const racersData: Record<string, Racer> = {};
+
+      querySnapshot.forEach((doc) => {
+        const data = doc.data() as Racer;
+        racersData[data.chipNumber] = data;
+      });
+
+      setRacers(racersData);
+    };
+
+    fetchRacers();
+  }, []);
+
   return (
     <Box sx={{ mt: 3 }}>
       <Typography variant="h6" gutterBottom>
@@ -22,15 +49,22 @@ const LapTimesTable: React.FC<LapTimesTableProps> = ({ lapTimes }) => {
           <TableRow>
             <TableCell>Lap</TableCell>
             <TableCell>Time</TableCell>
+            <TableCell>Nickname</TableCell>
+            <TableCell>Race Number</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {lapTimes.map((lapTime, index) => (
-            <TableRow key={index}>
-              <TableCell>{lapTime.lap}</TableCell>
-              <TableCell>{lapTime.time}</TableCell>
-            </TableRow>
-          ))}
+          {lapTimes.map((lapTime, index) => {
+            const racer = racers[lapTime.chipNumber] || { nickname: "Unknown", raceNumber: "N/A" };
+            return (
+              <TableRow key={index}>
+                <TableCell>{lapTime.lap}</TableCell>
+                <TableCell>{lapTime.time}</TableCell>
+                <TableCell>{racer.nickname}</TableCell>
+                <TableCell>{racer.raceNumber}</TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </Box>

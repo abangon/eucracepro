6733ed026ef4988af76621d31784cde38d552709
abangon <novotrek@gmail.com>
2025-02-26 18:55:51 +0100

    Update RaceTimingTable.tsx

--- src/components/RaceTimingTable.tsx
+++ src/components/RaceTimingTable.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../utils/firebase";
+import { db } from "@/utils/firebase";
 import { useParams } from "react-router-dom";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 
